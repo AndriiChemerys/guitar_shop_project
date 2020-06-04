fc466d5b6bfb47467385ad4f2518b0351986dd73
AndriiChemerys <andriichemerys@gmail.com>
2020-06-04 21:53:16 +0200

    Fixing update cart total func

--- script.js
+++ script.js
@@ -9,45 +9,45 @@
 let removeCartItemButtons = document.querySelectorAll('.btn-danger');
 
 removeCartItemButtons.forEach(element => {
-    let btn = element;
-    btn.addEventListener('click', removeCartItem);
+  let btn = element;
+  btn.addEventListener('click', removeCartItem);
 });
 
 let quantityInput = document.querySelectorAll('.cart-quantity-input')
 for (i of quantityInput) {
-    let input = i;
-    input.addEventListener('change', quantityChanged)
+  let input = i;
+  input.addEventListener('change', quantityChanged)
 }
 // });
 
 // Cart Clicked func
 let addToCartClicked = (event) => {
-    let btn = event.target
-    let shopItem = btn.parentElement.parentElement
-    let title = shopItem.querySelector('.card-title').innerText;
-    let price = shopItem.querySelector('.card-price').innerText;
-    let imageSrc = shopItem.querySelector('.card-image__pict').src;
-    console.log(title, price, imageSrc)
-    addItemToCart(title, price, imageSrc);
-    updateCartTotal()
+  let btn = event.target
+  let shopItem = btn.parentElement.parentElement
+  let title = shopItem.querySelector('.card-title').innerText;
+  let price = shopItem.querySelector('.card-price').innerText;
+  let imageSrc = shopItem.querySelector('.card-image__pict').src;
+  console.log(title, price, imageSrc)
+  addItemToCart(title, price, imageSrc);
+  updateCartTotal()
 }
 
 
 
 let addItemToCart = (title, price, imageSrc) => {
-    console.log(title, price, imageSrc)
-    let cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    let cartItems = document.querySelector('.cart-items')
-    let cartItemNames = cartItems.querySelectorAll('.cart-item-title')
-    for (i of cartItemNames) {
-        if (i.innerText == title) {
-            alert('This guitar is already added to the cart!')
-            return;
-        }
+  console.log(title, price, imageSrc)
+  let cartRow = document.createElement('div')
+  cartRow.classList.add('cart-row')
+  let cartItems = document.querySelector('.cart-items')
+  let cartItemNames = cartItems.querySelectorAll('.cart-item-title')
+  for (i of cartItemNames) {
+    if (i.innerText == title) {
+      alert('This guitar is already added to the cart!')
+      return;
     }
+  }
 
-    let cartRowContent = `
+  let cartRowContent = `
     <div class="">
     <div class="row row-bottom basket-item valign-wrapper">
       <div class="col s6">
@@ -73,10 +73,10 @@ let addItemToCart = (title, price, imageSrc) => {
       </div>
     </div>
   </div>`
-    cartRow.innerHTML = cartRowContent;
-    cartItems.append(cartRow);
-    cartRow.querySelector('.btn-danger').addEventListener('click', removeCartItem)
-    cartRow.querySelector('.cart-quantity-input').addEventListener('change', quantityChanged)
+  cartRow.innerHTML = cartRowContent;
+  cartItems.append(cartRow);
+  cartRow.querySelector('.btn-danger').addEventListener('click', removeCartItem)
+  cartRow.querySelector('.cart-quantity-input').addEventListener('change', quantityChanged)
 
 }
 
@@ -84,14 +84,14 @@ let addItemToCart = (title, price, imageSrc) => {
 let addToCartBtn = document.querySelectorAll('.shop-item-button');
 console.log(addToCartBtn);
 for (i of addToCartBtn) {
-    console.log(i);
-    i.addEventListener('click', addToCartClicked)
+  console.log(i);
+  i.addEventListener('click', addToCartClicked)
 }
 
 
 let removeCartItem = () => {
-    let btnClicked = event.target
-    btnClicked.parentElement.parentElement.remove();
+  let btnClicked = event.target
+  btnClicked.parentElement.parentElement.remove();
 }
 // let ready = () => {
 
@@ -99,31 +99,34 @@ let removeCartItem = () => {
 
 
 let quantityChanged = (event) => {
-    let input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 0;
-    }
-    console.log('Changed')
-    updateCartTotal();
+  let input = event.target
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 0;
+  }
+  console.log('Changed')
+  console.log(input.value)
+  updateCartTotal();
 }
 
 
 
 let updateCartTotal = () => {
-    let cartItemContainer = document.querySelector('.cart-items');
-    let cartRows = cartItemContainer.querySelectorAll('.cart-row');
-    let total;
-    for (let i of cartRows) {
-        let cartRow = i;
-        console.log(cartRow);
-        let priceEl = cartRow.querySelector('.cart-price')
-        let quantityElement = cartRow.querySelector('.cart-quantity-input')
-        let price = parseFloat(priceEl.innerText.replace('$', ''));
-        // console.log(price)
-        let quantity = quantityElement.value;
-        // console.log(price*quantity)
-        total += (price * quantity)
-    }
-    total = Math.round(total * 100) / 100
-    document.querySelector('.cart-total-price')
+  let cartItemContainer = document.querySelector('.cart-items');
+  let cartRows = cartItemContainer.querySelectorAll('.cart-row');
+  let total = 0;
+  for (let i of cartRows) {
+    let cartRow = i;
+    console.log(cartRow);
+    let priceEl = cartRow.querySelector('.cart-price')
+    let quantityElement = cartRow.querySelector('.cart-quantity-input')
+    let price = parseFloat(priceEl.innerText.replace('$', ''));
+    // console.log(price)
+    let quantity = quantityElement.value;
+    // console.log(price * quantity)
+    total = total + (price * quantity)
+    // console.log("total = " + total)
+  }
+  total = Math.round(total * 100) / 100
+  document.querySelector('.cart-total-price').innerText = '$' + total;
+  console.log("total = " + total)
 }
